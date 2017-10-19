279ac6a4e242202f29f714a334eda1ed57b533e8
seditaa <daniel.martinez.g1@estudiant.upc.edu>
2017-10-19 21:31:10 +0200

    Elements necessaris per la llista

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,11 +2,18 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList <String> itemList;
+    private ArrayAdapter <String> adapter;
 
     private ListView list;
     private EditText edit_item;
@@ -18,8 +25,16 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         list = (ListView) findViewById(R.id.list);
         edit_item = (EditText) findViewById(R.id.edit_item);
+
+        itemList = new ArrayList<>();
+        itemList.add("embotits");
+        itemList.add("gelats");
+        itemList.add("fruita");
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itemList);
+        list.setAdapter(adapter);
     }
 
     public void add_item(View view) {
+
     }
 }
