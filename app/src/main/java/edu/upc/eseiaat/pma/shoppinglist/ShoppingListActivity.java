e0b07ad710bd04d687c6c324da0d81de472be299
seditaa <daniel.martinez.g1@estudiant.upc.edu>
2017-10-19 21:11:36 +0200

    Layout creat

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,12 +2,24 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ListView list;
+    private EditText edit_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        list = (ListView) findViewById(R.id.list);
+        edit_item = (EditText) findViewById(R.id.edit_item);
+    }
+
+    public void add_item(View view) {
     }
 }
