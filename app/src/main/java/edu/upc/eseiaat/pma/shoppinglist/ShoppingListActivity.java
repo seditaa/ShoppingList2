a7e6f94d38c893f1ab70ae151107f6819845cacb
seditaa <daniel.martinez.g1@estudiant.upc.edu>
2017-10-28 00:58:01 +0200

    menú borrar elements marcats

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -145,5 +148,38 @@ public class ShoppingListActivity extends AppCompatActivity {
             edit_item.setText("");
         }
         list.smoothScrollToPosition(itemList.size()-1);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.clear_checked:
+                clearChecked();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void clearChecked() {
+        int i=0;
+        while (i<itemList.size()){
+            if (itemList.get(i).isChecked()){
+                itemList.remove(i);
+            }
+            else{
+                i++;
+            }
+        }
+        adapter.notifyDataSetChanged();
     }
 }
